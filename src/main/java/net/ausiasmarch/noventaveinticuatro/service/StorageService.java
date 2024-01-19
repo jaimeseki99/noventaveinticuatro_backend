2a00ffc8fd04001a2ed29736cd6dc2b5dc738b9b
Jaime <jaime99sq@gmail.com>
2024-01-19 12:40:15 +0100

    implementacion imagenes

---
+++ src/main/java/net/ausiasmarch/noventaveinticuatro/service/StorageService.java
@@ -0,0 +1,18 @@
+package net.ausiasmarch.noventaveinticuatro.service;
+
+import java.io.IOException;
+
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface StorageService {
+
+    void init();
+
+    String store(MultipartFile file);
+
+    Resource loadAsResource(String filename);
+    
+} 
